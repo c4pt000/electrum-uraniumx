f8ede8a7cb1cd909095882cf18a9f0ddd4304972
Your Name <you@example.com>
2021-08-09 15:54:38 -0400

    some changes to fedora-install.sh

--- fedora-install.sh
+++ fedora-install.sh
@@ -8,6 +8,10 @@ nano qrencode-devel qt5-qtbase-devel.x86_64 qt-devel qt4-devel wget google*fonts
 	git clone https://github.com/c4pt000/electrum-radiocoin
 	cd electrum-radiocoin
 
-	python3 -m pip install cryptography PyQt5
+	python3 -m pip install cryptography PyQt5 ecdsa
 	python3 -m pip install .
-	./run_electrum_radc 
+	echo "running electrum-radiocoin"
+	echo "./run_electrum"
+	echo ""
+	echo "requires port 50002 open for connectivity"
+	./run_electrum &
