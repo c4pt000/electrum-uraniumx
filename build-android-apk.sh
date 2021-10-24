e612b932f0badd698ffd4de879634bbe381ff643
Your Name <you@example.com>
2021-10-24 00:29:53 -0400

    rename radiowaves/b to geigers/b (for byte fee display accidental renaming)

---
+++ build-android-apk.sh
@@ -0,0 +1,5 @@
+docker run -it -d --rm \
+    -v $PWD:/home/user/wspace/electrum \
+    -v $PWD/.buildozer/.gradle:/home/user/.gradle \
+    --workdir /home/user/wspace/electrum \
+    electrum-android-builder-img
