98983091a2fd29e2d06b830e7c0827f318aa450e
Your Name <you@example.com>
2021-10-24 01:17:33 -0400

    repair for contrib/android builder see "build-android-apk.sh"

--- build-android-apk.sh
+++ build-android-apk.sh
@@ -1,5 +1,14 @@
+cd contrib/android
+docker build -t electrum-android-builder-img .
+
 docker run -it -d --rm \
     -v $PWD:/home/user/wspace/electrum \
     -v $PWD/.buildozer/.gradle:/home/user/.gradle \
     --workdir /home/user/wspace/electrum \
     electrum-android-builder-img
+echo "docker exec -it <docker_vm_hash> bash"
+echo "sudo -i"
+echo "pip install cython buildozer kivy"
+echo "cd /home/user/wspace/electrum"
+echo "./contrib/android/make_apk"
+echo ".apk will be located in dist"
